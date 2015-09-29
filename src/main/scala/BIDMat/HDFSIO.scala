9ffeca71cf059797a1b41a5c9e824cb13fefad2c
John Canny <jfc@cs.berkeley.edu>
2015-09-29 14:43:17 -0700

    added HDFSIO routine

---
+++ src/main/scala/BIDMat/HDFSIO.scala
@@ -0,0 +1,81 @@
+package BIDMat;
+import java.io.File
+import org.apache.hadoop.util.ReflectionUtils;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.SequenceFile.Reader;
+import org.apache.hadoop.io.SequenceFile.Writer;
+import org.apache.hadoop.io.SequenceFile.Metadata;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.IOUtils;
+import org.apache.hadoop.io.compress.DefaultCodec;
+import org.apache.hadoop.io.compress.SnappyCodec;
+
+object HDFSIO {
+  
+  def readMat(fname:String):Mat = {
+     val conf = new Configuration();
+     val path = new Path(fname);
+     var reader = new Reader(conf, Reader.file(path));
+     val key = new IntWritable;
+     val value = new MatIO;
+     reader.next(key, value);
+     IOUtils.closeStream(reader);
+     value.mat;
+  }
+  
+  def readND(fname:String):ND = {
+     val conf = new Configuration();
+     val path = new Path(fname);
+     var reader = new Reader(conf, Reader.file(path));
+     val key = new IntWritable;
+     val value = new NDIO;
+     reader.next(key, value);
+     IOUtils.closeStream(reader);
+     value.mat;
+  }
+  
+  def writeMat(fname:String, mat:Mat):Mat = {
+     val conf = new Configuration();
+     val fs = FileSystem.get(conf);
+     val path = new Path(fname);
+     val key = new IntWritable;
+     key.set(0);
+     val value = new MatIO;
+     value.mat = mat;
+     var writer = SequenceFile.createWriter(conf, 
+         Writer.file(path),
+         Writer.keyClass(key.getClass()),
+         Writer.valueClass(value.getClass()));
+/*         Writer.bufferSize(fs.getConf().getInt("io.file.buffer.size",4096)), 
+         Writer.blockSize(1073741824),
+         Writer.compression(SequenceFile.CompressionType.BLOCK, new DefaultCodec()),
+         Writer.progressable(null),
+         Writer.metadata(new Metadata()));      */
+
+     writer.append(key, value);
+     IOUtils.closeStream(writer);
+     mat;
+  }
+  
+   def writeND(fname:String, mat:ND):ND = {
+     val conf = new Configuration();
+     val fs = FileSystem.get(conf);
+     val path = new Path(fname);
+     val key = new IntWritable;
+     key.set(0);
+     val value = new NDIO;
+     value.mat = mat;
+     var writer = SequenceFile.createWriter(conf, 
+         Writer.file(path),
+         Writer.keyClass(key.getClass()),
+         Writer.valueClass(value.getClass()));
+
+     writer.append(key, value);
+     IOUtils.closeStream(writer);
+     mat;
+  }
+}
--- src/main/scala/BIDMat/MatIO.scala
+++ src/main/scala/BIDMat/MatIO.scala
@@ -1,15 +1,12 @@
-/**
- * This code was originally written by Derrick Cheng in 2013.
- */
+package BIDMat
+
 
 import org.apache.hadoop.io.Writable
 import java.io.IOException
 import java.io.DataOutput
 import java.io.DataInput
-import BIDMat.{Mat,FMat,IMat,SMat,BMat,DenseMat,SparseMat}
-import MatType._
 
-class MatIO extends Writable{
+class MatIO extends Writable {
   
   // include implicit empty constructor
   var contents : Mat = null
@@ -19,122 +16,57 @@ class MatIO extends Writable{
 	contents = m
   }
 
-  
-  
   override def write(out: DataOutput):Unit = {
     contents match {
-      case fM : FMat => writeDenseMat(fM, MatType.FMAT, (v:Float)=>out.writeFloat(v) ,out)
-      case iM : IMat => writeDenseMat(iM, MatType.IMAT, (v:Int)=>out.writeInt(v) ,out)
-      case sM : SMat => writeSparseMat(sM, MatType.SMAT,(v:Float)=>out.writeFloat(v) , out)
-      case bM : BMat => writeSparseMat(bM, MatType.BMAT,(v:Byte)=>out.writeByte(v) , out)
+      case fM:FMat => {out.writeInt(MatTypeTag.FMat); HMat.saveFMat(out, fM);}
     }
   }
   
-  override def readFields( in: DataInput):Unit = {
+  override def readFields(in: DataInput):Unit = {
     val matType : Int = in.readInt();
     matType match {
-      case MatType.FMAT => readDenseMat( (nr: Int, nc:Int, data0:Array[Float])=>FMat(nr,nc,data0), ()=>in.readFloat() , in)
-      case MatType.IMAT => readDenseMat( (nr: Int, nc:Int, data0:Array[Int])=>IMat(nr,nc,data0), ()=>in.readInt() , in)
-      case MatType.SMAT => readSparseMat( (nr: Int, nc: Int, nnz: Int, ir : Array[Int], jc : Array[Int], data : Array[Float])=>SMat(nr, nc, nnz, ir, jc, data), ()=>in.readFloat(), in)
-      case MatType.BMAT => readSparseMat( (nr: Int, nc: Int, nnz: Int, ir : Array[Int], jc : Array[Int], data : Array[Byte])=>BMat(nr, nc, nnz, ir, jc, data), ()=>in.readByte(), in)
+      case MatTypeTag.FMat => mat = HMat.loadFMat(in)
+    }
+  } 
+  
+}
 
-    }
-  }
+class NDIO extends Writable {
   
+  // include implicit empty constructor
+  var contents : ND = null
   
-  
-  private def writeDenseMat[T](dM : DenseMat[T] , matType : Int , writeT: T => Unit , out : DataOutput) {
-      out.writeInt(matType)
-	  out.writeInt(dM.nrows)
-	  out.writeInt(dM.ncols)
-	  var i : Int = 0
-	  while (i < dM.data.length)
-	  {
-	    writeT(dM.data(i))
-	    i = i+1
-	  }
-  }
-  
-  // http://www.scala-lang.org/node/133
-  // (x: Int, y: Int) => "(" + x + ", " + y + ")"
-  // multiparam for constructMat
-  // pass new DMat....
-  private def readDenseMat[T : Manifest](constructMat: (Int,Int,Array[T]) => DenseMat[T] , readT: () => T, in : DataInput)
-  {
-    val nrows : Int = in.readInt()
-    val ncols : Int = in.readInt()
-    val data : Array[T] = new Array[T](nrows*ncols)
-    var i : Int = 0
-    while (i < data.length)
-    {
-      data(i)=readT()
-      i = i+1
-	}
-    contents = constructMat(nrows,ncols,data)
-  }
-  
-  private def writeSparseMat[T](sM : SparseMat[T], matType : Int , writeT: T => Unit, out : DataOutput) {
-    out.writeInt(matType)
-    out.writeInt(sM.nrows)
-    out.writeInt(sM.ncols)
-    out.writeInt(sM.nnz)
-    var i, j, k = 0;
-    while (i < sM.ir.length)
-    {
-      out.writeInt(sM.ir(i))
-      i = i + 1;
-    }
-    while (j < sM.jc.length)
-    {
-      out.writeInt(sM.jc(j))
-      j = j + 1;
-    }
-    while (k < sM.data.length)
-    {
-      writeT(sM.data(k))
-      k = k + 1;
-    }
-  }
-  
-  private def readSparseMat[T : Manifest](constructMat: (Int,Int,Int,Array[Int],Array[Int],Array[T]) => SparseMat[T] , readT: () => T, in : DataInput)
-  {
-    val nrows : Int = in.readInt()
-    val ncols : Int = in.readInt()
-    var nnz : Int = in.readInt()
-    var ir = new Array[Int](nrows*ncols) // should be nnz but is nrows*ncols?
-    var jc = new Array[Int](ncols+1)
-    val data : Array[T] = new Array[T](nrows*ncols) // should be nnz but is ncols + 1?
-    
-    var i, j, k = 0;
-    while (i < ir.length)
-    {
-      ir(i) = in.readInt()
-      i = i + 1;
-    }
-    while (j < jc.length)
-    {
-      jc(j) = in.readInt();
-      j = j + 1;
-    }
-    while (k < data.length)
-    {
-      data(k) = readT()
-      k = k + 1;
-    }
-    contents = constructMat(nrows,ncols,nnz,ir,jc,data)
+  def mat = contents
+  def mat_=(m: ND) {
+  contents = m
   }
 
+  override def write(out: DataOutput):Unit = {
+    contents match {
+      case _ => throw new RuntimeException("NDwrite: type not matched");
+    }
+  }
   
+  override def readFields(in: DataInput):Unit = {
+    val matType : Int = in.readInt();
+    matType match {
+      case _ => throw new RuntimeException("NDread: type not matched");
+    }
+  } 
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+}
+
+object MatTypeTag {
+  final val IMat = 110;
+  final val LMat = 120;
+  final val FMat = 130;
+  final val DMat = 140;
+  final val SMat = 231;
+  final val SMatX = 331; // compressed indices
+  final val SDMat = 241;
+  final val SDMatX = 341; // compressed indices
+  final val SBMat = 201;
+  final val SBMatX = 301;
+  final val CSMat = 202;
+  final val CSMatX = 302;
 }
