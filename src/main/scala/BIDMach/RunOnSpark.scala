1258add82ecbc2aa6a069145d082fa756bc4335a
James Jia <jamesjia@berkeley.edu>
2016-04-07 10:59:35 -0700

    tmp

--- src/main/scala/BIDMach/RunOnSpark.scala
+++ src/main/scala/BIDMach/RunOnSpark.scala
@@ -13,50 +13,50 @@ import scala.reflect.ClassTag
 
 object RunOnSpark{
   // Called copy the reduced learner into a RDD of learners.
-  def mapToLearner(learner:Learner)(l: Iterator[Int]): Iterator[Learner] = {
-    Iterator[Learner](learner)
+  def mapToMockLearner(learner:MockLearner)(l: Iterator[Int]): Iterator[MockLearner] = {
+    Iterator[MockLearner](learner)
   }
 
   // Instantiates a learner based on the parameters of the learner that is passed in and binds the data to the new learner.
-  def firstPass(l: Learner)(rdd_data:Iterator[(SerText, BIDMat.MatIO)]):Iterator[Learner] = {
+  def firstPass(l: MockLearner)(rdd_data:Iterator[(SerText, BIDMat.MatIO)]):Iterator[MockLearner] = {
     val i_opts = new IteratorSource.Options
     i_opts.iter = rdd_data
     val iteratorSource = new IteratorSource(i_opts)
-    val learner = new Learner(iteratorSource, l.model, l.mixins, l.updater, l.datasink)
+    val learner = new MockLearner(iteratorSource, l.model, l.mixins, l.updater, l.datasink)
     learner.firstPass(null)
-    Iterator[Learner](learner)
+    Iterator[MockLearner](learner)
   }
 
   // Runs subsequent passes of the learner.
-  def nextPass(data_iterator: Iterator[(SerText, BIDMat.MatIO)], learner_iterator: Iterator[Learner]):Iterator[Learner] = {
+  def nextPass(data_iterator: Iterator[(SerText, BIDMat.MatIO)], learner_iterator: Iterator[MockLearner]):Iterator[MockLearner] = {
     val learner = learner_iterator.next
     learner.nextPass(data_iterator)
-    Iterator[Learner](learner)
+    Iterator[MockLearner](learner)
   }
 
-  def wrapUpLearner(learner_iterator: Iterator[Learner]): Iterator[Learner] = {
+  def wrapUpMockLearner(learner_iterator: Iterator[MockLearner]): Iterator[MockLearner] = {
     val learner = learner_iterator.next
     learner.wrapUp
-    Iterator[Learner](learner)
+    Iterator[MockLearner](learner)
   }
 
   // Gathers and reduces all the learners into one learner.
-  def reduce_fn(ipass: Int)(l: Learner, r: Learner): Learner = {
+  def reduce_fn(ipass: Int)(l: MockLearner, r: MockLearner): MockLearner = {
     l.model.combineModels(ipass, r.model)
     l
   }
 
-  def runOnSpark(sc: SparkContext, learner:Learner, rdd_data:RDD[(SerText,MatIO)], num_partitions: Int):RDD[Learner] = {
+  def runOnSpark(sc: SparkContext, learner:MockLearner, rdd_data:RDD[(SerText,MatIO)], num_partitions: Int):RDD[MockLearner] = {
     // Instantiate a learner, run the first pass, and reduce all of the learners' models into one learner.
-    var reduced_learner = rdd_data.mapPartitions[Learner](firstPass(learner), preservesPartitioning=true)
+    var reduced_learner = rdd_data.mapPartitions[MockLearner](firstPass(learner), preservesPartitioning=true)
                                   .treeReduce(reduce_fn(0), 2)
     // Once we've reduced our distributed learners into one learner, we can update our model.
     reduced_learner.updateM
 
     // Redistribute our reduced learner across all partitions
-    var rdd_learner: RDD[Learner] = sc.parallelize(1 to num_partitions)
+    var rdd_learner: RDD[MockLearner] = sc.parallelize(1 to num_partitions)
                                       .coalesce(num_partitions)
-                                      .mapPartitions[Learner](mapToLearner(reduced_learner), preservesPartitioning=true)
+                                      .mapPartitions[MockLearner](mapToMockLearner(reduced_learner), preservesPartitioning=true)
 
     rdd_learner.persist()
     
@@ -73,11 +73,11 @@ object RunOnSpark{
       // Redistribute the learner across all partitions
       rdd_learner = sc.parallelize(1 to num_partitions)
                       .coalesce(num_partitions)
-                      .mapPartitions[Learner](mapToLearner(reduced_learner), preservesPartitioning=true)
+                      .mapPartitions[MockLearner](mapToMockLearner(reduced_learner), preservesPartitioning=true)
       rdd_learner.persist()
     }
     // Note: The returned RDD has a transformation applied to it.
-    rdd_learner = rdd_learner.mapPartitions(wrapUpLearner, preservesPartitioning=true)
+    rdd_learner = rdd_learner.mapPartitions(wrapUpMockLearner, preservesPartitioning=true)
     rdd_learner
   }
 }
