6d22e8636bc75705b6c1184181320bf664181dad
James Jia <jamesjia@berkeley.edu>
2016-05-11 14:58:37 -0700

    Added imports to driver node

--- src/main/scala/BIDMach/RunOnSpark.scala
+++ src/main/scala/BIDMach/RunOnSpark.scala
@@ -12,6 +12,19 @@ import org.apache.spark.SparkContext
 import scala.reflect.ClassTag
 import org.apache.spark.util.SizeEstimator;
 import BIDMach.models.KMeans
+import BIDMat.{CMat,CSMat,DMat,Dict,FMat,FND,GMat,GDMat,GIMat,GLMat,GSMat,GSDMat,GND,HMat,IDict,Image,IMat,LMat,Mat,SMat,SBMat,SDMat}
+import BIDMat.MatFunctions._
+import BIDMat.SciFunctions._
+import BIDMat.Solvers._
+import BIDMat.Plotting._
+import BIDMach.Learner
+import BIDMach.models.{Click,FM,GLM,KMeans,KMeansw,LDA,LDAgibbs,Model,NMF,SFA,RandomForest,SVD}
+import BIDMach.networks.{Net}
+import BIDMach.datasources.{DataSource,MatSource,FileSource,SFileSource}
+import BIDMach.datasinks.{DataSink,MatSink}
+import BIDMach.mixins.{CosineSim,Perplexity,Top,L1Regularizer,L2Regularizer}
+import BIDMach.updaters.{ADAGrad,Batch,BatchNorm,Grad,IncMult,IncNorm,Telescoping}
+import BIDMach.causal.{IPTW}
 
 object RunOnSpark{
   // Called copy the reduced learner into a RDD of learners.
